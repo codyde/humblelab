d44fe4c9957f69d4923c8f7ccf404b3edd703d9b
CodyDe <codyde@thehumblelab.com>
2016-09-25 20:44:54 -0700

    Commiting Initial Projects

---
+++ Restful API - VMDB Python/python_vm_rest.py
@@ -0,0 +1,53 @@
+import pyodbc
+import json
+import collections
+from flask import Flask, jsonify
+from flask import abort
+
+objects_list = []
+conn =  pyodbc.connect(
+        'DSN=humblesql01;' # as specified in /etc/odbc.ini
+        'UID=sa;PWD=TestPass;')
+cursor = conn.cursor()
+
+app = Flask(__name__)
+
+def getdbjson():
+	global objects_list
+	cursor.execute("select * from otc.dbo.iaasmachine")
+	rows = cursor.fetchall() 
+	#objects_list = []
+	for row in rows:
+    		d = collections.OrderedDict()
+    		d['UID'] = row.uid
+    		d['hostname'] = row.hostname
+    		d['requestor'] = row.requestor
+    		d['orderNumber'] = row.orderNumber
+    		d['totalDisk'] = row.totalDisk
+    		d['requestCPU'] = row.requestCPU
+    		d['applicationRequest'] = row.applicationRequest
+    		d['requestStartDate'] = row.requestStartDate
+    		d['requestEndDate'] = row.requestEndDate
+    		d['iaascomputeCost'] = row.iaascomputeCost
+    		d['iaasstorageCost'] = row.iaasstorageCost
+    		d['capitalCost'] = row.capitalCost
+    		objects_list.append(d)
+	#j = json.dumps(objects_list, indent=4, sort_keys=True)
+	#print(j)
+
+@app.route('/humblelab/api/v1.0/vms', methods=['GET'])
+def get_vms():
+	getdbjson()
+	return jsonify({'vms': objects_list})
+
+@app.route('/humblelab/api/v1.0/vms/<string:hostname>', methods=['GET'])
+def get_hostname(hostname):
+	getdbjson()
+	host = [host for host in objects_list if host['hostname'] == hostname]
+	if len(host) == 0:
+		abort(404)
+	return jsonify({'hostname': host[0]})
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
